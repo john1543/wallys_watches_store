db206e911b43e15aaad0a2248be2903b400c4edf
John Yadao <jyadao@academic.rrc.ca>
2013-11-15 15:13:49 -0600

    First Commit for Wally's Watches Store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WallysWatchesStore::Application.config.secret_token = 'a6293a35a31f0a53b0338755ac13c2a388785db14a6890aa771977249cdd9b6fb7fdd887d995a0ec68e8d774d36e0210e71f54a7991b6bd7f2cb4b74ec48041f'
