663dc12df46666bfedc09131043638cd3b7e4377
John Yadao <jyadao@academic.rrc.ca>
2013-11-15 15:17:27 -0600

    Commit for products model

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ActiveRecord::Base
+  attr_accessible :description, :name, :price, :stock_quantity
+  
+  validates :description, :name, :price, :stock_quantity, :presence => true
+  validates :price, :numericality => true
+end
