47f5551f9fd02b702a4881f71d03fd442aae364d
John Yadao <jyadao@academic.rrc.ca>
2013-12-02 06:38:22 -0600

    commit for active admin registered with models

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   attr_accessible :customer_id, :gst_rate, :hst_rate, :pst_rate, :status
   belongs_to :customer
   has_many :lineitem
-  has_many :product, :through => appointments
+  has_many :product, :through => :lineitem
   
   validates :status, :customer_id, :presence => true
   validates :customer_id, :gst_rate, :hst_rate, :pst_rate, :numericality => true
