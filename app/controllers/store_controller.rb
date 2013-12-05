c3b1d75838174d122683b0261650c0789a390aae
John Yadao <jyadao@academic.rrc.ca>
2013-12-05 07:41:45 -0600

    consistent look and feel

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,12 @@ class StoreController < ApplicationController
     @categories = Category.all
     @sale = Product.first
   end
+  def about
+    #code
+  end
+  def contact
+    #code
+  end
   
   def search_results    
     #@products = Product.where("name LIKE '%#{params[:keywords]}%' AND category_id = #{params[:search_category_names]}")
